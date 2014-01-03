3e9587f682d235f1d8e4dd68236413120b42be92
PaulStoffregen <paul@pjrc.com>
2014-01-03 14:25:06 -0800

    Add AudioStream update_stop

--- teensy3/AudioStream.h
+++ teensy3/AudioStream.h
@@ -127,6 +127,7 @@ protected:
 	audio_block_t * receiveReadOnly(unsigned int index = 0);
 	audio_block_t * receiveWritable(unsigned int index = 0);
 	static bool update_setup(void);
+	static void update_stop(void);
 	static void update_all(void) { NVIC_SET_PENDING(IRQ_SOFTWARE); }
 	friend void software_isr(void);
 	friend class AudioConnection;
