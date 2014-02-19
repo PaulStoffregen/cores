252bb0da893e33b1d1148f7a0599812da22b5a6b
Andrew J. Kroll <a@oo.ms>
2014-02-19 02:04:05 -0500

    BUGFIX: make main weak, so that it may be overridden

--- teensy/main.cpp
+++ teensy/main.cpp
@@ -1,12 +1,13 @@
 #include <WProgram.h>
 
 //int main(void) __attribute__((noreturn));
+int main(void) __attribute__((weak));
 int main(void)
 {
 	_init_Teensyduino_internal_();
 
 	setup();
-    
+
 	while (1) {
 		loop();
 	}
