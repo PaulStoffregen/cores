2e2ce610c3a649d610883d25a1c702a75cb91a09
PaulStoffregen <paul@pjrc.com>
2014-06-28 04:04:12 -0700

    DMAChannel stuff (work in progess - scatter/gather)

--- teensy3/DMAChannel.cpp
+++ teensy3/DMAChannel.cpp
@@ -4,10 +4,9 @@
 // so C-only code can reserve DMA channels
 uint16_t dma_channel_allocated_mask = 0;
 
-DMAChannel::DMAChannel(uint8_t channelRequest) : TCD(*(TCD_t *)0), channel(16)
+void DMAChannel::init(void)
 {
-	uint8_t next, ch=channelRequest;
-
+	uint32_t ch = 0;
 	__disable_irq();
 	while (1) {
 		if (!(dma_channel_allocated_mask & (1 << ch))) {
@@ -15,9 +14,10 @@ DMAChannel::DMAChannel(uint8_t channelRequest) : TCD(*(TCD_t *)0), channel(16)
 			__enable_irq();
 			break;
 		}
-		next = (ch + 1) & 15;
-		if (next == channelRequest) {
+		if (++ch >= DMA_NUM_CHANNELS) {
 			__enable_irq();
+			TCD = (TCD_t *)0;
+			channel = 16;
 			return; // no more channels available
 			// attempts to use this object will hardfault
 		}
@@ -30,12 +30,69 @@ DMAChannel::DMAChannel(uint8_t channelRequest) : TCD(*(TCD_t *)0), channel(16)
 	DMA_CERR = ch;
 	DMA_CEEI = ch;
 	DMA_CINT = ch;
+	TCD = (TCD_t *)(0x40009000 + ch * 32);
+	uint32_t *p = (uint32_t *)TCD;
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 0;
+}
 
-	TCD = *(TCD_t *)(0x40009000 + ch * 32);
-	TCD.CSR = 0;
-	TCD.ATTR = 0;
-	TCD.NBYTES = 0;
-	TCD.BITER = 0;
-	TCD.CITER = 0;
+void DMAChannel::release(void)
+{
+	if (channel >= DMA_NUM_CHANNELS) return;
+	DMA_CERQ = channel;
+	__disable_irq();
+	dma_channel_allocated_mask &= ~(1 << channel);
+	__enable_irq();
+	channel = 16;
+	TCD = (TCD_t *)0;
+}
+
+static uint32_t priority(const DMAChannel &c)
+{
+	uint32_t n;
+	n = *(uint32_t *)((uint32_t)&DMA_DCHPRI3 + (c.channel & 0xFC));
+	n = __builtin_bswap32(n);
+	return (n >> ((c.channel & 0x03) << 3)) & 0x0F;
+}
+
+static void swap(DMAChannel &c1, DMAChannel &c2)
+{
+	uint8_t c;
+	DMABaseClass::TCD_t *t;
+
+	c = c1.channel;
+	c1.channel = c2.channel;
+	c2.channel = c;
+	t = c1.TCD;
+	c1.TCD = c2.TCD;
+	c2.TCD = t;
+}
+
+void DMAPriorityOrder(DMAChannel &ch1, DMAChannel &ch2)
+{
+	if (priority(ch1) < priority(ch2)) swap(ch1, ch2);
+}
+
+void DMAPriorityOrder(DMAChannel &ch1, DMAChannel &ch2, DMAChannel &ch3)
+{
+	if (priority(ch2) < priority(ch3)) swap(ch2, ch3);
+	if (priority(ch1) < priority(ch2)) swap(ch1, ch2);
+	if (priority(ch2) < priority(ch3)) swap(ch2, ch3);
+}
+
+void DMAPriorityOrder(DMAChannel &ch1, DMAChannel &ch2, DMAChannel &ch3, DMAChannel &ch4)
+{
+	if (priority(ch3) < priority(ch4)) swap(ch3, ch4);
+	if (priority(ch2) < priority(ch3)) swap(ch2, ch3);
+	if (priority(ch1) < priority(ch2)) swap(ch1, ch2);
+	if (priority(ch3) < priority(ch4)) swap(ch2, ch3);
+	if (priority(ch2) < priority(ch3)) swap(ch1, ch2);
+	if (priority(ch3) < priority(ch4)) swap(ch2, ch3);
 }
 
