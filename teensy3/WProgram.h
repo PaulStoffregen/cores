2f8709c6c934a929bd8a6833601af8e5b7d6f225
PaulStoffregen <paul@pjrc.com>
2016-06-13 10:41:08 -0700

    Include WCharacter.h by default

--- teensy3/WProgram.h
+++ teensy3/WProgram.h
@@ -34,7 +34,7 @@
 #include "usb_touch.h"
 #include "usb_undef.h" // do not allow usb_desc.h stuff to leak to user programs
 
-//#include "WCharacter.h"
+#include "WCharacter.h"
 #include "WString.h"
 #include "elapsedMillis.h"
 #include "IntervalTimer.h"
