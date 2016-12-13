2b24e39e130a4fa5ef276e474e2c594ddadecd3a
PaulStoffregen <paul@pjrc.com>
2016-12-13 00:43:34 -0800

    Improve bitWrite macro

--- teensy3/wiring.h
+++ teensy3/wiring.h
@@ -109,7 +109,7 @@ extern "C"{
 #define bitRead(value, bit) (((value) >> (bit)) & 0x01)
 #define bitSet(value, bit) ((value) |= (1UL << (bit)))
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#define bitWrite(value, bit, bitvalue) ((bitvalue) ? bitSet((value), (bit)) : bitClear((value), (bit)))
 
 typedef unsigned int word;
 
