399e79e5b31c6d834ba34475d23c719a34c1d7e0
Frank <FrankBoesing@users.noreply.github.com>
2017-01-22 14:57:55 +0100

    __cxa_guard_acquire - match declarations in mk20dx128.c
    - got a compilerwarning with GCC 6 and LTO
    This fixes the warning.
    I don't know wether GCC5 prints that warning, too.

--- teensy3/new.h
+++ teensy3/new.h
@@ -16,8 +16,8 @@ void operator delete[](void * ptr);
 
 __extension__ typedef int __guard __attribute__((mode (__DI__)));
 
-extern "C" int __cxa_guard_acquire(__guard *);
-extern "C" void __cxa_guard_release (__guard *);
+extern "C" int __cxa_guard_acquire(char *g);
+extern "C" void __cxa_guard_release (char *g);
 extern "C" void __cxa_guard_abort (__guard *);
 extern "C" void __cxa_pure_virtual(void);
 
