528c6e51823f9e704c9c6cee829a84a77c389326
tni <tni@users.noreply.github.com>
2017-05-11 02:22:15 +0200

    Add missing SPCR definitions. (#243)

--- teensy3/avr_emulation.cpp
+++ teensy3/avr_emulation.cpp
@@ -33,12 +33,16 @@
 #include "SPIFIFO.h"
 
 uint8_t SPCRemulation::pinout = 0;
+SPCRemulation SPCR;
 #if defined(KINETISL)
 uint8_t SPCR1emulation::pinout = 0;
+SPCR1emulation SPCR1;
 #endif
 #if defined(__MK64FX512__) || defined(__MK66FX1M0__)
 uint8_t SPCR1emulation::pinout = 0;
+SPCR1emulation SPCR1;
 uint8_t SPCR2emulation::pinout = 0;
+SPCR2emulation SPCR2;
 #endif
 #ifdef HAS_SPIFIFO
 
