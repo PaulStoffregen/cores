68aecd945adbc94a786c2499ed58d2e438233f12
PaulStoffregen <paul@pjrc.com>
2017-05-30 17:21:35 -0700

    map function support for floats and improved integer scaling

--- teensy3/WMath.cpp
+++ teensy3/WMath.cpp
@@ -40,11 +40,6 @@ int32_t random(int32_t howsmall, int32_t howbig)
 	return random(diff) + howsmall;
 }
 
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-{
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 unsigned int makeWord(unsigned int w) { return w; }
 unsigned int makeWord(unsigned char h, unsigned char l) { return (h << 8) | l; }
 
