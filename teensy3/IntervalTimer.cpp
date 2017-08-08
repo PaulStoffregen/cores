4a0aebb7837984344807ef7239b14d3fa2f6cc85
Frank <FrankBoesing@users.noreply.github.com>
2017-08-08 22:37:22 +0200

    end() never disables interrupts
    attention, patch for KINETISK only

--- teensy3/IntervalTimer.cpp
+++ teensy3/IntervalTimer.cpp
@@ -84,6 +84,7 @@ bool IntervalTimer::beginCycles(void (*funct)(), uint32_t cycles)
 void IntervalTimer::end() {
 	if (channel) {
 		int index = channel - KINETISK_PIT_CHANNELS;
+		NVIC_DISABLE_IRQ(IRQ_PIT_CH0 + index);
 		funct_table[index] = dummy_funct;
 		channel->TCTRL = 0;
 #if defined(KINETISL)
