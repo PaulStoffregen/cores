96a7ab14dd5ed5736da1334ee4c643bcb3ae3214
PaulStoffregen <paul@pjrc.com>
2017-09-03 13:17:06 -0700

    Opps, fix pgm_read_ptr_near & pgm_read_ptr_far

--- teensy3/avr/pgmspace.h
+++ teensy3/avr/pgmspace.h
@@ -121,11 +121,11 @@ typedef uint64_t prog_uint64_t;
 #define pgm_read_word_near(addr) pgm_read_word(addr)
 #define pgm_read_dword_near(addr) pgm_read_dword(addr)
 #define pgm_read_float_near(addr) pgm_read_float(addr)
-#define pgm_read_ptr_near(addr) pgm_read_float(addr)
+#define pgm_read_ptr_near(addr) pgm_read_ptr(addr)
 #define pgm_read_byte_far(addr) pgm_read_byte(addr)
 #define pgm_read_word_far(addr) pgm_read_word(addr)
 #define pgm_read_dword_far(addr) pgm_read_dword(addr)
 #define pgm_read_float_far(addr) pgm_read_float(addr)
-#define pgm_read_ptr_far(addr) pgm_read_float(addr)
+#define pgm_read_ptr_far(addr) pgm_read_ptr(addr)
 
 #endif
