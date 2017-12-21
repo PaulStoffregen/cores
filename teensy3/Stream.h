62d8f8db45d071ee278b089ba3d9f7a1ed04db3e
PaulStoffregen <paul@pjrc.com>
2017-12-21 11:43:44 -0800

    Support String input in Stream find functions

--- teensy3/Stream.h
+++ teensy3/Stream.h
@@ -34,12 +34,20 @@ class Stream : public Print
 	void setTimeout(unsigned long timeout);
 	bool find(const char *target);
 	bool find(const uint8_t *target) { return find ((const char *)target); }
+	bool find(const String &target) { return find(target.c_str()); }
 	bool find(const char *target, size_t length);
 	bool find(const uint8_t *target, size_t length) { return find ((const char *)target, length); }
+	bool find(const String &target, size_t length) { return find(target.c_str(), length); }
 	bool findUntil(const char *target, const char *terminator);
 	bool findUntil(const uint8_t *target, const char *terminator) { return findUntil((const char *)target, terminator); }
+	bool findUntil(const String &target, const char *terminator) { return findUntil(target.c_str(), terminator); }
+	bool findUntil(const char *target, const String &terminator) { return findUntil(target, terminator.c_str()); }
+	bool findUntil(const String &target, const String &terminator) { return findUntil(target.c_str(), terminator.c_str()); }
 	bool findUntil(const char *target, size_t targetLen, const char *terminate, size_t termLen);
 	bool findUntil(const uint8_t *target, size_t targetLen, const char *terminate, size_t termLen) {return findUntil((const char *)target, targetLen, terminate, termLen); }
+	bool findUntil(const String &target, size_t targetLen, const char *terminate, size_t termLen);
+	bool findUntil(const char *target, size_t targetLen, const String &terminate, size_t termLen);
+	bool findUntil(const String &target, size_t targetLen, const String &terminate, size_t termLen);
 	long parseInt();
 	long parseInt(char skipChar);
 	float parseFloat();
