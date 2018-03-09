5b8d6d44d6e3e22b684f70dfd6e5d08554c7a8c4
PaulStoffregen <paul@pjrc.com>
2018-03-09 03:42:20 -0800

    Reduce startup delay slightly

--- teensy3/pins_teensy.c
+++ teensy3/pins_teensy.c
@@ -583,9 +583,15 @@ void _init_Teensyduino_internal_(void)
 	// for background about this startup delay, please see these conversations
 	// https://forum.pjrc.com/threads/36606-startup-time-(400ms)?p=113980&viewfull=1#post113980
 	// https://forum.pjrc.com/threads/31290-Teensey-3-2-Teensey-Loader-1-24-Issues?p=87273&viewfull=1#post87273
+#if TEENSYDUINO >= 142
+	delay(25);
+	usb_init();
+	delay(275);
+#else
 	delay(50);
 	usb_init();
 	delay(350);
+#endif
 }
 
 
