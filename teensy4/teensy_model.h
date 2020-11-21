70c8d64bded005227b69495e3ad18a9f2b348061
Frank <FrankBoesing@users.noreply.github.com>
2020-11-21 17:04:04 +0100

    Create teensy_model.h

---
+++ teensy4/teensy_model.h
@@ -0,0 +1,9 @@
+// This header file is in the public domain.
+
+#if defined( ARDUINO_TEENSY_MICROMOD )
+#define TEENSY_MODEL "Teensy MicroMod"
+#elif defined( ARDUINO_TEENSY41 )
+#define TEENSY_MODEL "Teensy 4.1"
+#elif defined ( ARDUINO_TEENSY40 )
+#define TEENSY_MODEL "Teensy 4.0"
+#endif
