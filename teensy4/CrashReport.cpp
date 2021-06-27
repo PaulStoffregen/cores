bd18a75201a63059675e07dffcb8b7d62dfd3d44
PaulStoffregen <paul@pjrc.com>
2021-06-26 20:46:01 -0700

    CrashReport message format and troubleshooting advice

--- teensy4/CrashReport.cpp
+++ teensy4/CrashReport.cpp
@@ -11,6 +11,8 @@ struct arm_fault_info_struct {
         uint32_t bfar;
         uint32_t ret;
         uint32_t xpsr;
+        float  temp;
+        uint32_t time;
         uint32_t crc;
 }; */
 extern unsigned long _ebss;
@@ -20,33 +22,29 @@ extern "C" uint32_t set_arm_clock(uint32_t frequency); // clockspeed.c
 static int isvalid(const struct arm_fault_info_struct *info);
 static void cleardata(struct arm_fault_info_struct *info);
 
+FLASHMEM
 size_t CrashReportClass::printTo(Print& p) const
 {
   struct arm_fault_info_struct *info = (struct arm_fault_info_struct *)0x2027FF80;
 
   if (isvalid(info)) {
-    p.println("CrashReport:");	
-	p.print("  Fault occurred at: ");
-	uint8_t ss = info->time % 60;
-	info->time /= 60;
-	uint8_t mm = info->time % 60;
-	info->time /= 60;
-	uint8_t hh = info->time % 24;
-	p.printf( "%02d:%02d:%02d\n", hh, mm, ss  );
-
-	p.println("  Temperature and System Clock at time of fault: ");
-	p.printf( "    Temp = %f deg C,  Sys Clock = %u Mhz\n" , info->temp, F_CPU_ACTUAL/1000000 );
-
-    p.print("  length: ");
-    p.println(info->len);	
-    p.print("  IPSR: ");
-    p.println(info->ipsr, HEX);
+    p.println("CrashReport:");
+    uint8_t ss = info->time % 60;
+    info->time /= 60;
+    uint8_t mm = info->time % 60;
+    info->time /= 60;
+    uint8_t hh = info->time % 24;
+    p.printf("  A problem occurred at (system time) %02d:%02d:%02d\n", hh, mm, ss);
+    p.printf("  Code was executing from address %08X\n", info->ret);
+    //p.print("  length: ");
+    //p.println(info->len);
+    //p.print("  IPSR: ");
+    //p.println(info->ipsr, HEX);
 
     uint32_t _CFSR = info->cfsr;
     if (_CFSR > 0) {
       p.print("  CFSR: ");
       p.println(info->cfsr, HEX);
-      if ((_CFSR & 0xff) > 0) {
       //Memory Management Faults
       if ((_CFSR & 1) == 1) {
         p.println("\t(IACCVIOL) Instruction Access Violation");
@@ -60,45 +58,50 @@ size_t CrashReportClass::printTo(Print& p) const
         p.println("\t(MLSPERR) MemMange Fault on FP Lazy State");
       }
       if (((_CFSR & (0x80)) >> 7) == 1) {
-        p.print("\t(MMARVALID) Accessed Address: 0x");
-        p.print(info->mmfar, HEX);
-        if (info->mmfar < 32) p.print(" (nullptr)");
-        if ((info->mmfar >= (uint32_t)&_ebss) && (info->mmfar < (uint32_t)&_ebss + 32))
-        p.print(" (Stack problem)\n\tCheck for stack overflows, array bounds, etc.");
+        p.printf("\t(MMARVALID) Accessed Address: %08X", info->mmfar);
+        if (info->mmfar < 32) {
+          p.printf(" (nullptr)\n\t  Check code at %08X - very likely a bug!\n"
+            "\t  Run \"addr2line -e mysketch.ino.elf 0x%X\" for filename & line number.",
+            info->ret, info->ret);
+            // TODO: in some perfect future, maybe we'll build part of the ELF debug_line
+            // section (maybe just the .ino files) into CrashReport and be able to report
+            // the actual filename and line number.  Wouldn't that be awesome?!
+        } else if ((info->mmfar >= (uint32_t)&_ebss) && (info->mmfar < (uint32_t)&_ebss + 32)) {
+          p.print(" (Stack problem)\n\t  Check for stack overflows, array bounds, etc.");
+        }
         p.println();
-      }
       }
       //Bus Fault Status Register BFSR
       if (((_CFSR & 0x100) >> 8) == 1) {
-      p.println("\t(IBUSERR) Instruction Bus Error");
+        p.println("\t(IBUSERR) Instruction Bus Error");
       } else  if (((_CFSR & (0x200)) >> 9) == 1) {
-      p.println("\t(PRECISERR) Data bus error(address in BFAR)");
+        p.println("\t(PRECISERR) Data bus error(address in BFAR)");
       } else if (((_CFSR & (0x400)) >> 10) == 1) {
-      p.println("\t(IMPRECISERR) Data bus error but address not related to instruction");
+        p.println("\t(IMPRECISERR) Data bus error but address not related to instruction");
       } else if (((_CFSR & (0x800)) >> 11) == 1) {
-      p.println("\t(UNSTKERR) Bus Fault on unstacking for a return from exception");
+        p.println("\t(UNSTKERR) Bus Fault on unstacking for a return from exception");
       } else if (((_CFSR & (0x1000)) >> 12) == 1) {
-      p.println("\t(STKERR) Bus Fault on stacking for exception entry");
+        p.println("\t(STKERR) Bus Fault on stacking for exception entry");
       } else if (((_CFSR & (0x2000)) >> 13) == 1) {
-      p.println("\t(LSPERR) Bus Fault on FP lazy state preservation");
+        p.println("\t(LSPERR) Bus Fault on FP lazy state preservation");
       }
       if (((_CFSR & (0x8000)) >> 15) == 1) {
-      p.print("\t(BFARVALID) Accessed Address: 0x");
-      p.println(info->bfar, HEX);
+        p.print("\t(BFARVALID) Accessed Address: 0x");
+        p.println(info->bfar, HEX);
       }
       //Usage Fault Status Register UFSR
       if (((_CFSR & 0x10000) >> 16) == 1) {
-      p.println("\t(UNDEFINSTR) Undefined instruction");
+        p.println("\t(UNDEFINSTR) Undefined instruction");
       } else  if (((_CFSR & (0x20000)) >> 17) == 1) {
-      p.println("\t(INVSTATE) Instruction makes illegal use of EPSR)");
+        p.println("\t(INVSTATE) Instruction makes illegal use of EPSR)");
       } else if (((_CFSR & (0x40000)) >> 18) == 1) {
-      p.println("\t(INVPC) Usage fault: invalid EXC_RETURN");
+        p.println("\t(INVPC) Usage fault: invalid EXC_RETURN");
       } else if (((_CFSR & (0x80000)) >> 19) == 1) {
-      p.println("\t(NOCP) No Coprocessor");
+        p.println("\t(NOCP) No Coprocessor");
       } else if (((_CFSR & (0x1000000)) >> 24) == 1) {
-      p.println("\t(UNALIGNED) Unaligned access UsageFault");
+        p.println("\t(UNALIGNED) Unaligned access UsageFault");
       } else if (((_CFSR & (0x2000000)) >> 25) == 1) {
-      p.println("\t(DIVBYZERO) Divide by zero");
+        p.println("\t(DIVBYZERO) Divide by zero");
       }
     }
 
@@ -116,53 +119,64 @@ size_t CrashReportClass::printTo(Print& p) const
       }
     }
 
-    p.print("  MMFAR: ");
-    p.println(info->mmfar, HEX);
-    p.print("  BFAR: ");
-    p.println(info->bfar, HEX);
-    p.print("  return address: ");
-    p.println(info->ret, HEX);
-    p.print("  XPSR: ");
-    p.println(info->xpsr, HEX);
-    p.print("  crc: ");
-    p.println(info->crc, HEX);
+    p.printf("  Temperature inside the chip was %.1f °C\n" , info->temp);
+
+    // TODO: fault handler should read the CCM & PLL registers to log actual speed at crash
+    p.printf("  Startup CPU clock speed is %u MHz\n", F_CPU_ACTUAL/1000000);
+
+
+    //p.print("  MMFAR: ");
+    //p.println(info->mmfar, HEX);
+    //p.print("  BFAR: ");
+    //p.println(info->bfar, HEX);
+    //p.print("  return address: ");
+    //p.println(info->ret, HEX);
+    //p.print("  XPSR: ");
+    //p.println(info->xpsr, HEX);
+    //p.print("  crc: ");
+    //p.println(info->crc, HEX);
   } else {
-    p.println("No Crash Reported or Crash was Cleared");
+    p.println("No Crash Data To Report");
+    p.println("  Hopefully all is well, but certain types of crashes can't be reported:");
+    p.println("\tstuck in an infinite loop (technically, hardware still running properly)");
+    p.println("\tremaining in a low power sleep mode");
+    p.println("\taccess to certain peripherals without their clock enabled (eg, FlexIO)");
+    p.println("\tchange of CPU or bus clock speed without use of glitchless mux");
   }
   uint32_t SRSR = SRC_SRSR;
   if (SRSR & SRC_SRSR_LOCKUP_SYSRESETREQ) {
     // use SRC_GPR5 to distinguish cases.  See pages 1290 & 1294 in ref manual
     uint32_t gpr5 = SRC_GPR5;
     if (gpr5 == 0x0BAD00F1) {
-      p.println("Reboot was caused by 8 second auto-reboot after fault or bad interrupt detected");
+      p.println("  Reboot was caused by auto reboot after fault or bad interrupt detected");
     } else {
-      p.println("Reboot was caused by software write to SCB_AIRCR or CPU lockup");
+      p.println("  Reboot was caused by software write to SCB_AIRCR or CPU lockup");
     }
   }
   if (SRSR & SRC_SRSR_CSU_RESET_B) {
-    p.println("Reboot was caused by security monitor");
+    p.println("  Reboot was caused by security monitor");
   }
   if (SRSR & SRC_SRSR_IPP_USER_RESET_B) {
     // This case probably can't occur on Teensy 4.x
     // because the bootloader chip monitors 3.3V power
     // and manages DCDC_PSWITCH and RESET, causing the
     // power on event to appear as a normal reset.
-    p.println("Reboot was caused by power on/off button");
+    p.println("  Reboot was caused by power on/off button");
   }
   if (SRSR & SRC_SRSR_WDOG_RST_B) {
-    p.println("Reboot was caused by watchdog 1 or 2");
+    p.println("  Reboot was caused by watchdog 1 or 2");
   }
   if (SRSR & SRC_SRSR_JTAG_RST_B) {
-    p.println("Reboot was caused by JTAG boundary scan");
+    p.println("  Reboot was caused by JTAG boundary scan");
   }
   if (SRSR & SRC_SRSR_JTAG_SW_RST) {
-    p.println("Reboot was caused by JTAG debug");
+    p.println("  Reboot was caused by JTAG debug");
   }
   if (SRSR & SRC_SRSR_WDOG3_RST_B) {
-    p.println("Reboot was caused by watchdog 3");
+    p.println("  Reboot was caused by watchdog 3");
   }
   if (SRSR & SRC_SRSR_TEMPSENSE_RST_B) {
-    p.println("Reboot was caused by temperature sensor");
+    p.println("  Reboot was caused by temperature sensor");
 	  SRC_SRSR &= ~0x100u; /* Write 0 to clear. */
 	  p.println("Panic Temp Exceeded Shutting Down");
 	  p.println("Can be caused by Overclocking w/o Heatsink or other unknown reason");
@@ -175,12 +189,14 @@ size_t CrashReportClass::printTo(Print& p) const
   return 1;
 }
 
+FLASHMEM
 void CrashReportClass::clear()
 {
   struct arm_fault_info_struct *info = (struct arm_fault_info_struct *)0x2027FF80;
   cleardata(info);
 }
 
+FLASHMEM
 CrashReportClass::operator bool()
 {
 	struct arm_fault_info_struct *info = (struct arm_fault_info_struct *)0x2027FF80;
@@ -188,6 +204,7 @@ CrashReportClass::operator bool()
 	return false;
 }
 
+FLASHMEM
 static int isvalid(const struct arm_fault_info_struct *info)
 {
 	uint32_t i, crc;
@@ -205,6 +222,7 @@ static int isvalid(const struct arm_fault_info_struct *info)
 	return 1;
 }
 
+FLASHMEM
 static void cleardata(struct arm_fault_info_struct *info)
 {
 	info->len = 0;
