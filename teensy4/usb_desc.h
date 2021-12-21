87f650ceb210b6accc0a1be37507d99aab97965e
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2021-12-21 21:42:21 +0000

    Add dual serial+MIDI+audio option
    Useful for testing OSCAudio. Needs change to boards.txt: see comment in usb_desc.h, as this is a couple of levels up from the cores directory.

--- teensy4/usb_desc.h
+++ teensy4/usb_desc.h
@@ -515,7 +515,58 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   #define ENDPOINT2_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_INTERRUPT
   #define ENDPOINT3_CONFIG	ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
   #define ENDPOINT4_CONFIG	ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
-
+  
+#elif defined(USB_MIDI_AUDIO_DUAL_SERIAL)
+// To get the menu option, add the following into \Arduino\hardware\teensy\avr\boards.txt:
+//   teensy41.menu.usb.serial2midiaudio=Dual Serial + MIDI + Audio
+//   teensy41.menu.usb.serial2midiaudio.build.usbtype=USB_MIDI_AUDIO_DUAL_SERIAL
+	#define VENDOR_ID 0x16C0
+	#define PRODUCT_ID 0x048B
+	#define MANUFACTURER_NAME {'T','e','e','n','s','y','d','u','i','n','o'}
+	#define MANUFACTURER_NAME_LEN 11
+	#define PRODUCT_NAME {'T','e','e','n','s','y',' ','D','S','e','r','i','a','l','/','M','I','D','I','/','A','u','d','i','o'}
+	#define PRODUCT_NAME_LEN 25
+	#define EP0_SIZE 64
+	#define NUM_ENDPOINTS 8
+	#define NUM_INTERFACE 8
+	#define CDC_IAD_DESCRIPTOR 1
+	#define CDC_STATUS_INTERFACE 0
+	#define CDC_DATA_INTERFACE 1 // Serial
+	#define CDC_ACM_ENDPOINT 2
+	#define CDC_RX_ENDPOINT 3
+	#define CDC_TX_ENDPOINT 3
+	#define CDC_ACM_SIZE 16
+	#define CDC_RX_SIZE_480 512
+	#define CDC_TX_SIZE_480 512
+	#define CDC_RX_SIZE_12 64
+	#define CDC_TX_SIZE_12 64
+	#define CDC2_STATUS_INTERFACE 2 // SerialUSB1
+	#define CDC2_DATA_INTERFACE 3
+	#define CDC2_ACM_ENDPOINT 4
+	#define CDC2_RX_ENDPOINT 5
+	#define CDC2_TX_ENDPOINT 5
+	#define MIDI_INTERFACE 4 // MIDI
+	#define MIDI_NUM_CABLES 1
+	#define MIDI_TX_ENDPOINT 6
+	#define MIDI_TX_SIZE_12 64
+	#define MIDI_TX_SIZE_480 512
+	#define MIDI_RX_ENDPOINT 6
+	#define MIDI_RX_SIZE_12 64
+	#define MIDI_RX_SIZE_480 512
+	#define AUDIO_INTERFACE 5 // Audio (uses 3 consecutive interfaces)
+	#define AUDIO_TX_ENDPOINT 7
+	#define AUDIO_TX_SIZE 180
+	#define AUDIO_RX_ENDPOINT 7
+	#define AUDIO_RX_SIZE 180
+	#define AUDIO_SYNC_ENDPOINT 8
+	#define ENDPOINT2_CONFIG ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_INTERRUPT
+	#define ENDPOINT3_CONFIG ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
+	#define ENDPOINT4_CONFIG ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_INTERRUPT
+	#define ENDPOINT5_CONFIG ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
+	#define ENDPOINT6_CONFIG ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
+	#define ENDPOINT7_CONFIG ENDPOINT_RECEIVE_ISOCHRONOUS + ENDPOINT_TRANSMIT_ISOCHRONOUS
+	#define ENDPOINT8_CONFIG ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_ISOCHRONOUS
+	
 #elif defined(USB_MIDI4_SERIAL)
   #define VENDOR_ID		0x16C0
   #define PRODUCT_ID		0x0489
