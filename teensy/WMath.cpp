5bccb1b631e5de0f5e290b2a44d60d417afbcf84
PaulStoffregen <paul@pjrc.com>
2024-01-13 15:22:12 -0800

    Fix makeWord() on Teensy 2.0

--- teensy/WMath.cpp
+++ teensy/WMath.cpp
@@ -56,5 +56,5 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-uint16_t makeWord(uint16_t w) { return w; }
-uint16_t makeWord(unsigned char h, unsigned char l) { return (h << 8) | l; }
+unsigned int /*uint16_t*/ makeWord(unsigned int /*uint16_t*/ w) { return w; }
+unsigned int /*uint16_t*/ makeWord(unsigned char h, unsigned char l) { return (h << 8) | l; }
