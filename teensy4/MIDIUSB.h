fd1634f93a810bcd38e5c823c6d819628ef7cc9e
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2025-11-07 11:56:42 +0000

    Add in a few more places where a USB mix would be broken
    Mixes including MIDI or extra serial ports wouldn't work properly.

--- teensy4/MIDIUSB.h
+++ teensy4/MIDIUSB.h
@@ -36,7 +36,7 @@
 #include "usb_midi.h"
 
 #ifdef __cplusplus
-#if !defined(USB_MIDI) && !defined(USB_MIDI4) && !defined(USB_MIDI16) && !defined(USB_MIDI_SERIAL) && !defined(USB_MIDI4_SERIAL) && !defined(USB_MIDI16_SERIAL) && !defined(USB_MIDI_AUDIO_SERIAL) && !defined(USB_MIDI16_AUDIO_SERIAL) && !defined(USB_EVERYTHING)
+#if !defined(MIDI_INTERFACE)
 #error "Please select MIDI in Tools > USB Type to use MIDIUSB.h"
 #endif
 
