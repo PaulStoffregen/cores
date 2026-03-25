f20cdcd9a81c49e5a6005893bec9154e464359c8
Shawn Silverman <shawn@pobox.com>
2026-03-25 11:29:24 -0700

    settimeofday: Assume tv->tv_usec can be anything, not just in [-1, 1000000]
    https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/sys_types.h.html
    says that suseconds_t is merely _at least_ in that range.

--- teensy4/rtc.c
+++ teensy4/rtc.c
@@ -93,7 +93,7 @@ __attribute__((weak))
 int settimeofday(const struct timeval *const tv,
                  const struct timezone *const tz __attribute__((unused))) {
   // Notes:
-  // * The type of tv_usec is suseconds_t, range is [-1, 1000000]
+  // * The type of tv_usec is suseconds_t, range is at least [-1, 1000000]
   // * There are 32768 ticks per 1,000,000 microseconds; that's where 512/15625
   //   comes from, it's 32768/1000000 in lowest terms
   // Refs:
@@ -107,12 +107,12 @@ int settimeofday(const struct timeval *const tv,
 
   uint32_t sec = (uint32_t)tv->tv_sec;
   uint32_t usec = (uint32_t)tv->tv_usec;
-  if (usec == 1000000) {
-    sec++;
-    usec = 0;
-  } else if (usec == UINT32_MAX) {
-    sec--;
-    usec = 999999;
+  if (tv->tv_usec >= 1000000) {
+    sec += tv->tv_usec / 1000000;
+    usec = tv->tv_usec % 1000000;
+  } else if (tv->tv_usec < 0) {
+    sec += tv->tv_usec / 1000000;
+    usec = 1000000 + (tv->tv_usec % 1000000);
   }
 
   const uint32_t hi = (sec >> 17) & 0x7fffu;
